6d8fa4398750d50e315e1c1f96b28e97dd577e94
monu <monu@LT-MBP-052.local>
2025-08-18 13:47:35 +0530

    tc01 with sign up module

--- locators/test/ayush_locator.py
+++ locators/test/ayush_locator.py
@@ -17,9 +17,23 @@ class AyushLocator:
     START_CHAT = (By.XPATH, "//button[@class='mbw-button-primary']")
     TYPE_MESSAGE_USER2ADVISOR = (By.XPATH, "//div[@class='formContainer--LzD2M']//textarea[@placeholder='Your message...']")
     SEND_MESSAGE_BUTTON_USER = (By.XPATH, "//button[normalize-space()='Send']")
-    USER_SIDE_ADVISOR_MESSAGE_VALIDATION = (By.XPATH, "//div[contains(text(),'Hello Sweet')]")
+    MIXPANEL_EMAIL = (By.XPATH, "//input[@placeholder='e.g. eleanor@mixpanel.com']")
+    MIXPANEL_PASSWORD = (By.XPATH, "//input[@placeholder='e.g. ····················']")
+    MIXPANEL_CONTINUE = (By.XPATH, "//button[@name='primaryLoginButton']")
+    MIXPANEL_LEFTPANEL = (By.XPATH, "//div[@class='_label-container_17b46_19']")
+    MIXPANEL_LEFTPANEL_PROJECT = (By.XPATH, "//span[@class='label' and text()='Purple Garden - Staging']")
+    MIXPANEL_USER = (By.XPATH, "//div[normalize-space()='Users']")
+    MIXPANEL_USER_SELECTION = (By.XPATH, "//div[title='anna.benishai+2705qa@ingenio.com']")
+    MIXPANEL_USER_CHAT=(By.XPATH, "//body[1]/mp-browser-context-root[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[2]/div[3]/div[2]/div[1]/div[1]/div[1]/profile-activity[1]/div[1]/div[2]/div[1]/div[1]/div[2]/mp-section[1]/div[1]")
 
-    # Advisor
+    # SignUp
+    JOIN = (By.XPATH, "//button[@class='loginButtonContainer--r7uit']")
+    RANDOM_EMAIL = (By.XPATH, "//input[@id='email']")
+    RETYPE_EMAIL = (By.XPATH, "//input[@id='confirmEmail']")
+    RANDOM_PASSWORD = (By.XPATH, "//input[@id='password']")
+    CREATE_ACCOUNT = (By.XPATH, "//button[@class='actionButton--UxfDy']")
+    TERMS_POLICY = (By.XPATH, "//button[@class='ppTofModalSignUpButton--Imglp']")
+    # advisor
     EMAIL_ADVISOR = (By.XPATH, "//input[@placeholder='Email']")
     PASSWORD_ADVISOR = (By.XPATH, "//input[@placeholder='Password']")
     SIGN_IN_BUTTON_ADVISOR = (By.XPATH, "//button[text()='LOG IN TO YOUR ACCOUNT']")
